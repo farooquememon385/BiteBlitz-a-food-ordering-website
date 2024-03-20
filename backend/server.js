b2beb2e60c21806006dee2de04cf28f61ce6e02e
farooquememon385 <farooquememon385@gmail.com>
2024-03-20 16:50:27 +0500

    added simple middleware

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,15 @@ require('dotenv').config()
 
 const express = require('express')
 
+// use postman to test APIs
+
 const app = express()
+
+// Middle ware
+app.use((req, resp, next) => {
+    console.log(req.path, req.method)
+    next()
+})
 
 app.get('/', (req, resp) =>{
     resp.json({msg: "Welcome to food ordering website"})
